e1b12a8304c7ccc0359d153179ba83e24e9f8427
Jaya Kasa <jaya.kasa@mongodb.com>
2022-06-16 21:10:47 -0400

    cleaned up Dockerfile and app.py

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,16 @@
 FROM ubuntu:20.04
 
 LABEL maintainer="bharadwajr07"
+WORKDIR /app
+
+# avoid stuck build due to user prompt
+ARG DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update -y && \
-    apt-get install -y python-pip python-dev
+    apt-get install -y python3.9 python3.9-dev python3-pip python3-wheel build-essential
 
 # We copy just the requirements.txt first to leverage Docker cache
-COPY ./requirements.txt /app/requirements.txt
-WORKDIR /app
+COPY requirements.txt .
 RUN pip install -r requirements.txt
-COPY . /app
-CMD [ "python", "./app.py" ]
+COPY . .
+CMD [ "python3", "app.py" ]
--- app.py
+++ app.py
@@ -2,27 +2,28 @@
 # 2. Make sure you have flask installed `pip install flask`
 # 3. Run using `flask run -h localhost -p 5000`
 
-from flask import Flask, request
+from flask import Flask, request, g
 
 app = Flask(__name__)
 name = "Bharadwaj"
+
 
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello {}'.format(name), 200
 
+
 @app.route('/set_name', methods=['POST'])
 def set_name():
-    global name
     try:
         payload = request.get_json()
         new_name = payload["name"]
         message = {
             "message": "Name changed!",
-            "old_name": name,
+            "old_name": g.name,
             "new_name": new_name,
         }
-        name = new_name
+        g.name = new_name
         return message, 201
     except Exception as e:
         return {"error": str(e)}, 500
